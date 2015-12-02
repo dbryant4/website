0ec2416b87880cbf0ac00d5d15cc220a8e106374
Derrick Bryant <dbryant4@gmail.com>
2015-12-01 17:06:45 -0800

    Add animated progress bar to indicate loading

--- site.js
+++ site.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   $.get("resume.md", function(data){
     $("div.resume").html(marked(data));
+  })
+  .fail(function(jqXHR, textStatus, errorThrown){
+    $("div.resume").html("Although the resume is not loading, you can still download a PDF version by using the Download button.");
   });
 });
